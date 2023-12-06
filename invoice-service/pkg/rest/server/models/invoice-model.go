f33a91dc19e08e10c7c1d83383fa16c3dd3de701
mahendraintelops <mahendra.b@intelops.dev>
2023-12-06 21:30:14 +0000

    commit by compage : generated files through compage for version: v1

---
+++ invoice-service/pkg/rest/server/models/invoice-model.go
@@ -0,0 +1,9 @@
+package models
+
+type Invoice struct {
+	Id int64 `json:"id,omitempty"`
+
+	Amount float32 `json:"amount,omitempty"`
+
+	PaymentTerms string `json:"paymentTerms,omitempty"`
+}
---
+++ invoice-service/pkg/rest/server/services/invoice-service.go
@@ -0,0 +1,36 @@
+package services
+
+import (
+	"github.com/mahendraintelops/intelops-procurement-solution-v3/invoice-service/pkg/rest/server/daos"
+	"github.com/mahendraintelops/intelops-procurement-solution-v3/invoice-service/pkg/rest/server/models"
+)
+
+type InvoiceService struct {
+	invoiceDao *daos.InvoiceDao
+}
+
+func NewInvoiceService() (*InvoiceService, error) {
+	invoiceDao, err := daos.NewInvoiceDao()
+	if err != nil {
+		return nil, err
+	}
+	return &InvoiceService{
+		invoiceDao: invoiceDao,
+	}, nil
+}
+
+func (invoiceService *InvoiceService) CreateInvoice(invoice *models.Invoice) (*models.Invoice, error) {
+	return invoiceService.invoiceDao.CreateInvoice(invoice)
+}
+
+func (invoiceService *InvoiceService) ListInvoices() ([]*models.Invoice, error) {
+	return invoiceService.invoiceDao.ListInvoices()
+}
+
+func (invoiceService *InvoiceService) GetInvoice(id int64) (*models.Invoice, error) {
+	return invoiceService.invoiceDao.GetInvoice(id)
+}
+
+func (invoiceService *InvoiceService) UpdateInvoice(id int64, invoice *models.Invoice) (*models.Invoice, error) {
+	return invoiceService.invoiceDao.UpdateInvoice(id, invoice)
+}
